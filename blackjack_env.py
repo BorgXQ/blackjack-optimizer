0fc997adda4165b56fe2d4088d7418f62b761259
Jonathan Widarsa <borgupine@Jonathans-Laptop.local>
2025-08-05 01:15:35 +0800

    Add deck class

--- blackjack_env.py
+++ blackjack_env.py
@@ -1,8 +1,9 @@
-
+import random
+from typing import Optional
 
 
 class Card:
-    """Represents a playing card."""
+    """Represents a playing card"""
 
     def __init__(self, suit: str, rank: str):
         self.suit = suit
@@ -12,10 +13,43 @@ class Card:
         return f"{self.rank} of {self.suit}"
     
     def value(self) -> int:
-        """Returns blackjack value of a card."""
+        """Returns blackjack value of a card"""
         if self.rank in ['J', 'Q', 'K']:
             return 10
         if self.rank == 'A':
             return 11 # Initialize to 11, adjusted later
         else:
             return int(self.rank)
+
+
+class Deck:
+    """Represents a deck of playing cards"""
+
+    def __init__(self, num_decks: int=1):
+        self.cards = []
+        self.num_decks = num_decks
+        self.reset_deck()
+
+    def reset_deck(self):
+        """Creates a fresh deck(s) of cards"""
+        self.cards = []
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+        for _ in range(self.num_decks):
+            for suit in suits:
+                for rank in ranks:
+                    self.cards.append(Card(suit, rank))
+
+        self.shuffle()
+
+    def shuffle(self):
+        """Shuffles the deck"""
+        random.shuffle(self.cards)
+
+    def deal_card(self) -> Optional[Card]:
+        """Deals one card from the deck"""
+        if len(self.cards) == 0:
+            return None
+        return self.cards.pop()
+    
